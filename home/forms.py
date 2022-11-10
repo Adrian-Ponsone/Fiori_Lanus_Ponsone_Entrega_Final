2ac0755ad4b05b2f4283d5b516108b9555043751
Adrian E Ponsone <adrianponsone@yahoo.com.ar>
2022-11-10 18:56:22 -0300

    Cambie la forma en la que se muestra como cargar la fecha correctamente en el formulario del auto

--- home/forms.py
+++ home/forms.py
@@ -8,9 +8,9 @@ class CarForm(forms.Form):
     car_type = forms.CharField(max_length=20, label='Sedan or QP')
     car_color = forms.CharField(max_length=20)
     car_year = forms.IntegerField()
-    fabrication_date = forms.DateField(required=False, label='Fabrication date (yyyy/mm/dd)')    
+    fabrication_date = forms.DateField(required=False, label='Fabrication date (mm/dd/yyyy)')    
     car_description = RichTextFormField(required=False)
-    posted_by = forms.CharField(required = False, max_length=20, label='type your username')
+    posted_by = forms.CharField(required = False, max_length=20, label='Posted By')
     car_image = forms.ImageField(required = False, label='Load an image')
         
 class SearchCarForm(forms.Form):
